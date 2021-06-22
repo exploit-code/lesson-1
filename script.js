08360afad09b8c1133b0b5923a7d7545087ca8b7
Borushko Alexander <o_xx@list.ru>
2021-06-22 12:58:42 +0300

    fixed pop-up menu

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const menuBtn = document.querySelector('.header__item-right--menu');
 const menu = document.querySelector('.menu');
 const menuCloseBtn = document.querySelector('.menu__btn-close');
+const header = document.querySelector('.header');
 
 menuBtn.addEventListener('click', () => {
     menu.classList.toggle('menu-hidden');
