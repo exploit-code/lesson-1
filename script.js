05fd0d83250dfc57041596fe5b5bd6b48268d93c
Borushko Alexander <o_xx@list.ru>
2021-06-24 19:21:56 +0300

    added responsive to the site

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const menuBtn = document.querySelector('.header__item-right--menu');
 const menu = document.querySelector('.menu');
 const menuCloseBtn = document.querySelector('.menu__btn-close');
-const header = document.querySelector('.header');
 
 menuBtn.addEventListener('click', () => {
     menu.classList.toggle('menu-hidden');
